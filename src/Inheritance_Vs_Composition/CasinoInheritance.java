72455e064f373bfbb93155c5b38ef219e5ad3816
witchrall <alex.uppstrom@hotmail.com>
2025-05-20 09:35:14 +0200

    Changes

---
+++ src/Inheritance_Vs_Composition/CasinoInheritance.java
@@ -0,0 +1,7 @@
+package se.kth.iv1350.amazingpos.Inheritance_Vs_Composition;
+
+import java.util.Random;
+
+public class CasinoInheritance extends Random{
+
+}
--- src/main/java/se/kth/iv1350/amazingpos/Inheritance_Vs_Composition/CasinoInheritance.java
+++
@@ -1,5 +0,0 @@
-package se.kth.iv1350.amazingpos.Inheritance_Vs_Composition;
-
-public class CasinoInheritance {
-
-}
