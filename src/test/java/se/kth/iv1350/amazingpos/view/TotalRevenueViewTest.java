4b1b8e635dc8268a340157bc46a8c1c83c01f0d7
witchrall <alex.uppstrom@hotmail.com>
2025-05-27 21:47:42 +0200

    Tests for Veiw and revenue view

--- src/test/java/se/kth/iv1350/amazingpos/view/TotalRevenueViewTest.java
+++ src/test/java/se/kth/iv1350/amazingpos/view/TotalRevenueViewTest.java
@@ -1,10 +1,46 @@
 package se.kth.iv1350.amazingpos.view;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TotalRevenueViewTest {
+    private TotalRevenueView totalRevenueView = new TotalRevenueView();
+    PrintStream originalOut;
+    ByteArrayOutputStream outContent;
+    private double revenue = 450;
+   
+    @BeforeEach
+    void setUp(){
+        outContent = new ByteArrayOutputStream();
+        originalOut = System.out;
+        System.setOut(new PrintStream(outContent));        
+    }
+
+    @AfterEach
+    void tearDownClass(){
+        System.setOut(originalOut);
+    }
+
     @Test
     void testDoShowTotalIncome() {
+        try{
+            totalRevenueView.doShowTotalIncome(revenue);
+        }catch(Exception e){
 
+        }
+
+        String output = outContent.toString();
+
+        assertTrue(output.contains("#### This is not part of the receipt ####"), "First line incorrect");
+        assertTrue(output.contains("#### Will be viewed on it's own GUI ####"), "Second line incorrect");
+        assertTrue(output.contains("####Total Revenue from todays sales####"), "Third line incorrect");
+        assertTrue(output.contains("Total revenue would be: 450.0"), "Fourth line incorrect");
+        assertTrue(output.contains("###################################"), "Last line incorrect");
     }
 }
--- src/test/java/se/kth/iv1350/amazingpos/view/ViewTest.java
+++ src/test/java/se/kth/iv1350/amazingpos/view/ViewTest.java
@@ -1,5 +1,10 @@
 package se.kth.iv1350.amazingpos.view;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,6 +21,9 @@ public class ViewTest {
     static  Printer printer;
     private View view;
 
+    PrintStream originalOut;
+    ByteArrayOutputStream outContent;
+
     @BeforeEach
     void setUpClass(){
         printer = new Printer();
@@ -23,6 +31,10 @@ public class ViewTest {
 
         this.contr = new Controller(externalSystems, printer);
         this.view = new View(contr);
+
+        outContent = new ByteArrayOutputStream();
+        originalOut = System.out;
+        System.setOut(new PrintStream(outContent));
     }
     @AfterEach
     void tearDownClass(){
@@ -30,10 +42,41 @@ public class ViewTest {
         externalSystems = null;
         contr = null;
         view = null;
+        System.setOut(originalOut);
     }
 
     @Test
-    void testRunFakeExecution() {
+    void testRunFakeExecutionContainsError() {
         view.runFakeExecution();
+        String output = outContent.toString();
+        assertTrue(output.contains("Invalid Item Id"), "Error message missing");
     }
+
+    @Test
+    void testRunFakeExecutionContainsItem1ItemDescriptions() {
+        view.runFakeExecution();
+        String output = outContent.toString();
+        assertTrue(output.contains("Item ID: 1"), "First line incorrect");
+        assertTrue(output.contains("Item name: Mjölk"), "Second line incorrect");
+        assertTrue(output.contains("Item cost: 150.0 SEK"), "Third line incorrect");
+        assertTrue(output.contains("VAT : 15.0%"), "Fourth incorrect");
+        assertTrue(output.contains("Item description: Arla laktosfri mjölk"), "Fifth line incorrect");
+        assertTrue(output.contains("Running total: 172.5 SEK"), "Sixth line incorrect");
+        assertTrue(output.contains("Total VAT: 22.5 SEK"), "Seventh line incorrect");
+    }
+
+    @Test
+    void testRunFakeExecutionContainsAmountToPay() {
+        view.runFakeExecution();
+        String output = outContent.toString();
+        assertTrue(output.contains("Amount to pay: 450.0"), "Amount to pay is incorrect or missing");
+    }
+
+    @Test
+    void testRunFakeExecutionContainsChange() {
+        view.runFakeExecution();
+        String output = outContent.toString();
+        assertTrue(output.contains("Change to give customer: 550.0"), "Change incorrect or missing");
+    }
+
 }
